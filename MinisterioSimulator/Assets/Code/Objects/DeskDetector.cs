7a8f2cfa1c477002216c99fd17371314b61ab164
Juancho <juan.rod707@gmail.com>
2017-08-05 14:25:05 -0300

    Agregue control, deteccion de interactuables y head bobbing

---
+++ MinisterioSimulator/Assets/Code/Objects/DeskDetector.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeskDetector : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Interactable"))
+        {
+            //Display Desk Action
+            Debug.Log("ShowTooltip");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag.Equals("Interactable"))
+        {
+            //Hide Desk Action
+            Debug.Log("HideTooltip");
+        }
+    }
+}
---
+++ MinisterioSimulator/Assets/Code/Visual/Oscillate.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillate : MonoBehaviour
+{
+    public float OscillatorFactor;
+    public float OscillatorFrequency;
+    public Axis axis;
+
+    private float angle;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    var oscillation = Vector3.zero;
+
+	    switch (axis)
+	    {
+	        case Axis.X:
+	            oscillation.x = OscillatorFactor * Mathf.Sin(angle);
+	            break;
+	        case Axis.Y:
+	            oscillation.y = OscillatorFactor * Mathf.Sin(angle);
+	            break;
+	        case Axis.Z:
+	            oscillation.z = OscillatorFactor * Mathf.Sin(angle);
+	            break;
+	    }
+
+	    this.transform.Translate(oscillation);
+	    this.angle += OscillatorFrequency;
+    }
+}
