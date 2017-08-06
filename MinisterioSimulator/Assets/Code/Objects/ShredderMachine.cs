c7f5e93a6b3910d05cc689b0c8d730e9d407d06d
Manuel <manu1502gmail.com>
2017-08-05 22:16:43 -0300

    Shredder 3d model and script.

---
+++ MinisterioSimulator/Assets/Code/Objects/ShredderMachine.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShredderMachine : MonoBehaviour {
+
+    Animator shredderAnimator;
+    ParticleSystem shrededParticles;
+    public float shredAnimationTime = 1.0f;
+    public float shredSpeedMofifier = 2.0f;
+
+	void Start () {
+        shredderAnimator = transform.GetChild(0).GetComponent<Animator>();
+        shrededParticles = transform.GetChild(0).GetChild(0).GetComponent<ParticleSystem>();
+        shrededParticles.Stop();
+    }
+
+    /*
+    private void Update() {
+        if (Input.GetKeyUp(KeyCode.T)) {
+            PlayShredAnimation();
+        }
+    }
+    */
+
+    public void PlayShredAnimation() {
+        shredderAnimator.SetFloat("speed", shredSpeedMofifier);
+        EmitParticles();
+    }
+
+    private void EmitParticles() {
+        shrededParticles.Play();
+        StartCoroutine(WaitActionCoroutine(shredAnimationTime));
+    }
+
+    IEnumerator WaitActionCoroutine(float waitTime) {
+        yield return new WaitForSeconds(waitTime);
+        shrededParticles.Stop();
+        shredderAnimator.SetFloat("speed", 1.0f);
+    }
+
+}
