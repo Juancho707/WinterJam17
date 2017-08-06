a53cff07f9255315e6154eef4e9ac5308581ef32
FEDERICO-MONTOL\federico.montoli <fedemc@gmail.com>
2017-08-06 09:09:34 -0300

    Iconos en pantalla para cada desk, stamp, copy

---
+++ MinisterioSimulator/Assets/Code/Visual/AutorotateIcon.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AutorotateIcon : MonoBehaviour
+{
+
+    private void Start()
+    {
+        var originalXRot = this.transform.eulerAngles.x;
+        this.transform.eulerAngles = new Vector3(originalXRot, 0f, 0f);
+    }
+
+}
