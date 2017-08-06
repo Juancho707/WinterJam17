e64c0c817d864b69577f05e733b9cc28aad44abb
FEDERICO-MONTOL\federico.montoli <fedemc@gmail.com>
2017-08-06 00:28:58 -0300

    Agregado randomizer de NPCs, y corregidos Inputs

---
+++ MinisterioSimulator/Assets/Code/Managers/NpcsResources.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class NpcsResources : MonoBehaviour
+{
+    public Sprite[] HeadsSpritesNPC;
+    public Sprite[] BodySpritesNPC;
+
+    public Sprite GetRandomHead()
+    {
+        return HeadsSpritesNPC.PickOne();
+    }
+
+    public Sprite GetRandomBody()
+    {
+        return BodySpritesNPC.PickOne();
+    }
+}
---
+++ MinisterioSimulator/Assets/Code/NPCs/NPCRandomizer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCRandomizer : MonoBehaviour
+{
+    public SpriteRenderer Head;
+    public SpriteRenderer Body;
+
+    private void Start()
+    {
+        Head.sprite= GlobalReferences.resources.GetComponent<NpcsResources>().GetRandomHead();
+        Body.sprite = GlobalReferences.resources.GetComponent<NpcsResources>().GetRandomBody();
+    }
+}
